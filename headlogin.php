8f6bd0b352fcd895d1545adad6a06803fbbacf47
Suhail8493 <UsernameSuhail0786@gmail.com>
2023-11-09 22:22:01 +0530

    Updated Head Login page

--- headlogin.php
+++ headlogin.php
@@ -4,139 +4,92 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php
-    session_start();
-    if (!isset($_SESSION['x']))
-        header("location:headlogin.php");
-
-    $conn = mysqli_connect("localhost", "root", "", "crime_portal");
-    if (!$conn) {
-        die("could not connect" . mysqli_error($conn));
-    }
-    mysqli_select_db($conn, "crime_portal");
-
-    if (isset($_POST['s1'])) {
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $cid = $_POST['cid'];
-            $_SESSION['cid'] = $cid;
-            header("location:head_case_details.php");
-        }
-    }
-
-    if (isset($_POST['s2'])) {
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $loc = $_POST['loc'];
-            $_SESSION['loc'] = $loc;
-            header("location:headHome1.php");
-        }
-    }
-
-    ?>
-    <title>Head Homepage</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css"
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet"
         type="text/css">
 
+    <title>Head Login</title>
+    <?php
 
-    <script>
-    function f1() {
-
-        var sta2 = document.getElementById("ciid").value;
-
-
-
-
-        var x2 = sta2.indexOf(' ');
-
-
-
-
-
-        if (sta2 != "" && x2 >= 0) {
-            document.getElementById("ciid").value = "";
-            alert("Blank Field Not Allowed");
+    if (isset($_POST['s'])) {
+        session_start();
+        $_SESSION['x'] = 1;
+        $conn = mysqli_connect("localhost", "root", "", "crime_portal");
+        if (!$conn) {
+            die("could not connect" . mysqli_error($conn));
         }
+        mysqli_select_db($conn, "crime_portal");
 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST['email'];
+            $pass = $_POST['password'];
+            $result = mysqli_query($conn, "SELECT h_id,h_pass FROM head where h_id='$name' and h_pass='$pass' ");
 
+            if (mysqli_num_rows($result) == 0) {
+                $message = "ID or Password Not Matched.";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+            } else {
+                header("location:headHome.php");
+            }
+        }
     }
-    </script>
+    ?>
 </head>
 
-<body style="background-image: url(search1.jpeg);">
+<body style="color: black;background-image: url(locker.jpeg);background-size: 100%;background-repeat: no-repeat;">
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+
                 <a class="navbar-brand" href="home.php"><b>Crime Portal</b></a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-
-                    <li><a href="headlogin.php" class="glyphicon glyphicon-arrow-left">
+                    <li><a href="home.php" class="glyphicon glyphicon-arrow-left">
 
                         </a></li>
-                    <li class="active"><a href="headHome.php">Head Home</a></li>
+                    <li class="active"><a href="headlogin.php">Head Login</a></li>
                 </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="headHome.php">View Complaints</a></li>
-                    <li><a href="head_view_police_station.php">Police Stations</a></li>
-                    <li><a href="h_logout.php">Logout &nbsp <i class="fa fa-sign-out" aria-hidden="true"></i></a></li>
-                </ul>
+
             </div>
         </div>
     </nav>
+    <div align="center">
+        <div class="form" style="margin-top: 15%">
+            <form method="post">
+                <div class="form-group" style="width: 30%">
+                    <label for="exampleInputEmail1">
+                        <h1 style="color:white">Head ID</h1>
+                    </label>
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1"
+                        aria-describedby="emailHelp" size="5" placeholder="Enter User ID" required>
+                </div>
+                <div class="form-group" style="width:30%">
+                    <label for="exampleInputPassword1">
+                        <h1 style="color:white">Password</h1>
+                    </label>
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1"
+                        placeholder="Password" required>
+                </div>
 
-    <div>
-        <form style="margin-top: 10%; margin-left: 40%;" method="post">
-            <input type="text" name="cid" style="width: 250px; height: 30px;" placeholder="&nbsp Complaint ID" id="ciid"
-                onfocusout="f1()" required>
-            <div>
-                <input class="btn btn-primary" type="submit" value="Search" name="s1"
-                    style="margin-top: 10px; margin-left: 11%;">
-            </div>
-        </form>
 
-        <form style="margin-top: 3%; margin-left: 40%;" method="post">
-            <select name="loc" class="form-control" style="width: 250px;">
-
-                <?php
-                $loc = mysqli_query($conn, "select location from police_station");
-                while ($row = mysqli_fetch_array($loc)) {
-                    ?>
-                <option>
-                    <?php echo $row[0]; ?>
-                </option>
-                <?php
-                }
-                ?>
-            </select>
-
-            <input class="btn btn-primary" type="submit" value="Search" name="s2"
-                style="margin-top: 10px; margin-left: 11%;">
-        </form>
+                <button type="submit" class="btn btn-primary" name="s">Submit</button>
+            </form>
+        </div>
     </div>
-
     <div style="position: fixed;
    left: 0;
    bottom: 0;
    width: 100%;
-   background-color: rgba(0,0,0,0.5);
+   height: 30px;
+   background-color: rgba(0,0,0,0.8);
    color: white;
    text-align: center;">
         <h4 style="color: white;">&copy <b>Crime Portal 2020(Kashmir)</b></h4>
     </div>
-
-
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
-    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </body>
 
